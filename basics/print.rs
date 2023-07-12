0da7373493b2b3e554f0d9a5cbb736757a1b6de0
roshan <45297269+bad-logic@users.noreply.github.com>
2023-07-12 09:38:28 +0545

    print variants

---
+++ basics/print.rs
@@ -0,0 +1,22 @@
+fn main() {
+
+    // print variants
+    println!("Hello, world!");
+
+    print!("hello, world!");
+
+    print!("\n");
+
+    print!("The value is {}",10);
+
+    print!("\n");
+
+    // carriage return \r 
+    print!("This is not displayed \r this will be displayed");
+
+    print!("\n");
+
+    println!("I am {1} happy to do {0}","this","very");
+
+    println!("I am {arg1} happy to do {arg2}",arg1="very",arg2="this");
+}
