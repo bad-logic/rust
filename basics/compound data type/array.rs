b42161ce273ee9e5254cf7fe4966c2d2bbf214bc
roshan <45297269+bad-logic@users.noreply.github.com>
2023-07-14 20:18:45 +0545

    compound data type

---
+++ basics/compound data type/array.rs
@@ -0,0 +1,39 @@
+fn main(){
+    let mut number_array:[i32;5] = [4,5,6,7,9];
+
+    println!("{}",number_array[0]);
+
+    println!("{:?}",number_array);
+
+    number_array[4] = 5;
+    println!("{:?}",number_array);
+
+    let array_with_same_element:[i32;10] = [0;10];
+    let mut string_array_1:[&str;3] = ["apple","tomato","grapes"];
+
+    let string_array_2:[&str;6]=["unknown";6];
+    string_array_1[0] = "hello there";
+
+
+    let char_array:[char;5]=['a','p','p','l','e'];
+
+    let mut number_array_1:[i32;5] = [4,5,6,8,9];
+
+    let subset_array:&[i32] = &number_array_1[0..=3];
+
+    println!("{:?} {:?} {:?} {:?} {:?} {:?}",
+    array_with_same_element,
+    string_array_1,
+    string_array_2,
+    char_array,
+    number_array_1,
+    subset_array );
+
+    println!("The length of number_array_1 is {}",number_array_1.len());
+    println!("The array is occupying {} bytes",std::mem::size_of_val(&number_array));
+
+    let check_index: Option<&i32> = number_array_1.get(100);
+
+    println!("{:?}",check_index);
+
+}
---
+++ basics/compound data type/string.rs
@@ -0,0 +1,58 @@
+//-------------------------------------------
+//      Strings
+//          - Fixed length strings (&str)
+//          - Growable Strings (String)
+//---------------------------------------------
+
+fn main(){
+    let some_string: &str = "Fixed length string";
+    
+    let mut growable_string = String::from("This string will grow");
+
+    println!("The string is \"{}\"",growable_string);
+
+    growable_string.push('s');
+    println!("The string is \"{}\"",growable_string);
+
+    growable_string.pop();
+    println!("The string is \"{}\"",growable_string);
+
+    growable_string.push_str(" which i will use");
+    println!("The string is \"{}\"",growable_string);
+
+    println!(
+        "Basic functions on strings,
+        is_empty(): {},
+        length(): {},
+        Bytes(): {},
+        contains use: {}",
+        growable_string.is_empty(),
+        growable_string.len(),
+        growable_string.capacity(),
+        growable_string.contains("use")
+    );
+
+    growable_string.push_str("   ");
+    let str_len =growable_string.trim().len();
+
+    let number = 6;
+    let num_str = number.to_string();
+
+    println!("Is the number a string: {}",num_str=="6");
+
+    let some_char: char = 'a';
+    let char_str:String = some_char.to_string();
+
+    let my_name:String = "bhattarai".to_string();
+
+    let empty_string = String::new();
+
+    println!("Length is {}", empty_string.len());
+
+    let s1:String = "Bhattarai".to_string();
+    let s2:String = "Roshan".to_string();
+
+    let s3:String = format!("{} {}",s2,s1);
+
+    println!("{}",s3);
+}
