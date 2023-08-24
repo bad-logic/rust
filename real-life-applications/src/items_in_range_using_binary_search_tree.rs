bce46c40968f7e5eaa251de7635d1133ca3eeb55
roshan <mailme.roshanbhattarai@gmail.com>
2023-08-24 11:12:32 +0545

    range items using binary search tree

---
+++ real-life-applications/src/items_in_range_using_binary_search_tree.rs
@@ -0,0 +1,87 @@
+//----------------------------------
+//      Items in Range
+//          - Description
+//              - Given a vector of product prices, search for items in a given price range
+//              
+// 
+//          - Tools
+//              - Binary Search Tree, Box Pointer
+//----------------------------------
+
+#[derive(Clone)]
+#[derive(Debug)]
+struct Node{
+    val: i32,
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>
+}
+
+impl Node{
+    fn new(value:i32)->Self{
+        Node { val: value, left: None, right: None }
+    }
+
+    fn insert(&mut self, value:i32){
+        if value > self.val {
+            // go to the right of the node
+            match self.right{
+                None => self.right = Some(Box::new(Node::new(value))),
+                Some(ref mut node) => node.insert(value)
+            }
+        }else{
+            match self.left {
+                None => self.left = Some(Box::new(Node::new(value))),
+                Some(ref mut node) => node.insert(value)
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct BinarySearchTree{
+    root:Node
+}
+
+fn tree_traversal(node:Option<Box<Node>>, low:i32,high:i32,mut output:&mut Vec<i32>){
+    if !node.is_none(){
+
+        if node.as_ref().unwrap().val >= low && node.as_ref().unwrap().val <= high{
+            output.push(node.as_ref().unwrap().val);
+        }
+    
+        if node.as_ref().unwrap().val >= low {
+            // traverse left
+            tree_traversal(node.as_ref().unwrap().left.clone(), low, high,&mut output);
+        }
+    
+        if node.as_ref().unwrap().val <= high {
+            // traverse right
+            tree_traversal(node.as_ref().unwrap().right.clone(), low, high, &mut output);
+        }
+
+    }
+}
+
+fn products_in_range(root:Node,low:i32,high:i32)->Vec<i32>{
+    let mut output = Vec::new();
+    tree_traversal(Some(Box::new(root)), low, high, &mut output);
+    output
+
+}
+
+fn main() {
+    let product_prices = vec![9,6,14,20,1,30,8,17,5];
+    let mut binarySearchTree:BinarySearchTree = BinarySearchTree{
+        root: Node::new(product_prices[0])
+    };
+
+    for i in 1..product_prices.len(){
+        binarySearchTree.root.insert(product_prices[i]);
+    }
+
+    println!("{:?}",binarySearchTree);
+
+    let range_prices = products_in_range(binarySearchTree.root,7,20);
+    println!("products in price ranges: {:?}",range_prices);
+    
+}
