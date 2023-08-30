e8efc962c21235d3bf7ecbe802410c0d781e51c3
roshan <45297269+bad-logic@users.noreply.github.com>
2023-08-30 20:19:52 +0545

    initializing structs

--- structs/src/bin/initializing_struct_instances.rs
+++ structs/src/bin/initializing_struct_instances.rs
@@ -4,7 +4,20 @@
 // 
 //-----------------------------------
 
-
+mod lib;
 
 fn main(){
+
+    // let new_student = Student{
+    //     id:222,
+    //     name: "Bob".to_string(),
+    //     age:20
+    // };
+
+    let new_student = lib::Student::new("Bob12".to_string())
+    .unwrap_or_default();
+
+    println!("{:?}", new_student);
+
+    
 }
